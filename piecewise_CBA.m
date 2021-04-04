45e4a143eea30c7330bb270f611e697f83ab272d
Zion <guzhaoyuan14@gmail.com>
2021-04-03 21:39:55 -0400

    Debug CBA.

--- piecewise_CBA.m
+++ piecewise_CBA.m
@@ -11,7 +11,7 @@ M           =gv.M;       %  M    =   ro_arm*diag([I J J A A A]); % mass matrix o
 xi_star     =gv.xi_star;
 Gra         =gv.Gra;
 dX          =gv.dX;      %  dX   = L/(num_disc-1);
-X           =gv.X;       %   X   = linspace(0,L,num_piece);        % [m]
+X           =gv.X;       %  X   = linspace(0,L,num_piece+1);        % [m]
 tact        =gv.tact;    %  loading time
 trel        =gv.trel;    %  relaxing time
 Fax         =gv.Fax;
@@ -28,7 +28,7 @@ Fpmy        =gv.Fpmy;
 Fpmz        =gv.Fpmz;
 g           =gv.g;
 eta         =gv.eta;
-nstep       =gv.nstep; 
+nstep       =gv.nstep;
 
 % Code for getting parameters.
 
@@ -43,17 +43,62 @@ Tau         = zeros(6*num_piece,1);      % Internal force and actuation force L(
 GM          = zeros(6*num_piece,6);      % Gravitational matrix
 ECL         = zeros(6*num_piece,1);      % External Concentrated Load
 
-for zz=1:length(t)
 
-Xi          =z(1:6*num_piece,zz);
-Xidot       =z(6*num_piece+1:12*num_piece,zz);
+
+Xi          =z(1:6*num_piece);
+Xidot       =z(6*num_piece+1:12*num_piece);
 g_r         =[0 -1 0 0; 1 0 0 0; 0 0 1 0; 0 0 0 1];    % cantilever
 g_prec      =diag([1 1 1 1]); %初始位形
 eta_prec    =zeros(6,1);
+g_prec_list = [g_prec];
+eta_prec_list = [eta_prec];
 
-for m = 1:num_piece 
+% Record current joint states.
+xi_array = [];
+theta_array = [];
+xidot_array = [];
+for iii = 1:N
+    
+    % xi and theta in every piece
 
-    for n = 1:num_piece 
+    xin         = Xi(6*(iii-1)+1:6*(iii-1)+6,:);     % xi and theta in centain section, produced and saved           
+    xidotn      = Xidot(6*(iii-1)+1:6*(iii-1)+6,:);           
+    kn          = xin(1:3);
+    thetan      = sqrt(kn'*kn);
+
+    xi_array    = [xi_array,xin];
+    xidot_array = [xidot_array,xidotn];
+    theta_array = [theta_array,thetan];
+    
+    X1 = X(iii);
+    for ii=1:num_disc
+        X1 = X1 + dX;
+        invAdjg    =piecewise_invAdjoint(X1,thetan,xin);
+
+        intdAdjg   =piecewise_ADJ(X1,thetan,xin);
+
+    %-----所有disc的位置组成g矩阵-----------%一个disc对应是4×4的矩阵，这样一个g就会有4×disc×piece列（位置）
+    % g有4×n行，因为每一个时刻就会有一条构型曲线。（时间）
+    %每一个disc在不同时刻的位置和速度
+        g(4*(nstep-1)+1:4*(nstep-1)+4,4*(iii-1)*num_disc+4*(ii-1)+1:4*(iii-1)*num_disc+4*(ii-1)+4)... %%
+            =g_r*g_prec*piecewise_expmap(X1,thetan,xin);
+        eta(6*(nstep-1)+1:6*(nstep-1)+6,(iii-1)*num_disc+ii)...
+            =invAdjg*(eta_prec+intdAdjg*xidotn);
+    end
+    invAdjgn_last   =piecewise_invAdjoint(X(iii+1),thetan,xin);
+    intdAdjgn_last  =piecewise_ADJ(X(iii+1),thetan,xin);
+    g_prec          =g_prec*piecewise_expmap(X(iii+1),thetan,xin);%%---position
+    ADxin           =intdAdjgn_last*xidotn;
+    eta_prec        =invAdjgn_last*(eta_prec+ADxin);  %%--speed
+    
+    g_prec_list = [g_prec_list, g_prec]; % 4x4(N+1).
+    eta_prec_list = [eta_prec_list, eta_prec]; % 6x(N+1).
+end
+
+% CBA.
+for m = 1:num_piece
+
+    for n = 1:num_piece
     
         M_mn   = zeros(6);
         C1_mn  = zeros(6);
@@ -61,32 +106,17 @@ for m = 1:num_piece
         Tau_m  = zeros(6,1);
         G_m    = zeros(6,6);
         ECL_m  = zeros(6,1);
-        xi_array = [];
-        theta_array = [];
-        xidot_array = [];
-        
-        for iii = 1:N  
-            
-            % xi and theta in every piece
-            
-            xin         = Xi(6*(iii-1)+1:6*(iii-1)+6,:);     % xi and theta in centain section, produced and saved           
-            xidotn      = Xidot(6*(iii-1)+1:6*(iii-1)+6,:);           
-            kn          = xin(1:3);        
-            thetan      = sqrt(kn'*kn);
-           
-            xi_array    =   [xi_array,xin];
-            xidot_array = [xidot_array,xidotn];
-            theta_array = [theta_array,thetan];
-            
-        end
         
         for i = max(m,n):N
             
             L_i_1 = X(i);                  %  index of X     X=linspace(0,L,num_piece);
             
             X1    = L_i_1;                 %  L_i_1=L_{i-1};
-                        
-            for ii = 1:num_disc          
+            M_mn_i = zeros(6);
+            C1_mn_i = zeros(6);
+            C2_mn_i = zeros(6);
+            G_m_i = zeros(6);
+            for ii = 1:num_disc
             
                 X1 = X1 + dX;              %  The actual position of disc.
                 
@@ -100,76 +130,75 @@ for m = 1:num_piece
                     
                     intdAdjg       = piecewise_ADJ(X1,theta_array(:,jj),xi_array(:,jj));          %%  T_g
                     
-                    Adjg           = piecewise_Adjoint(X1,theta_array(:,jj),xi_array(:,jj));
+                    Adjg           = piecewise_Adjoint(X1,theta_array(:,jj),xi_array(:,jj));      %%  Ad_g
                        
                     invAdjg_array  = [invAdjg_array,invAdjg];
                     
                     intdAdjg_array = [intdAdjg_array,intdAdjg];
                     
                     Adjg_array     = [Adjg_array,Adjg];
-                     
+                    
                 end
-                                    
-                g(4*(nstep-1)+1:4*(nstep-1)+4,4*(jj-1)*num_disc+4*(ii-1)+1:4*(jj-1)*num_disc+4*(ii-1)+4)...
-                        =g_r*g_prec*piecewise_expmap(X1,theta_array(:,jj),xi_array(:,jj));
-                    
-                eta(6*(nstep-1)+1:6*(nstep-1)+6,(jj-1)*num_disc+ii)...
-                        =invAdjg*(eta_prec+intdAdjg*xidot_array(:,jj));
-               
-%                   g_prec         = g_prec*piecewise_expmap(X1,theta_array(:,jj),xi_array(:,jj));   %considering time
-                    
-%                   eta_prec       = invAdjg*(eta_prec+intdAdjg*xidot_array(:,jj));
+%                                     
+%                 g(4*(nstep-1)+1:4*(nstep-1)+4,4*(jj-1)*num_disc+4*(ii-1)+1:4*(jj-1)*num_disc+4*(ii-1)+4)...
+%                         =g_r*g_prec*piecewise_expmap(X1,theta_array(:,jj),xi_array(:,jj));
+%                 
+%                 eta(6*(nstep-1)+1:6*(nstep-1)+6,(jj-1)*num_disc+ii)...
+%                         =invAdjg*(eta_prec+intdAdjg*xidot_array(:,jj));
+%                
+%                 
+% %                   eta_prec       = invAdjg*(eta_prec+intdAdjg*xidot_array(:,jj));
 
                 % generate Jacobian matrix
           
-                J_i = [];                                  %  Equation 20
+                J_i = [];                              %  Equation 20
                 
-                    for j = i:-1:1                         %  non-zero items in certain piece           
-                        
-                        item = intdAdjg_array(:,i-j+1);   
-                        
-                        for k = 1:j                       
-                            
-                            item = invAdjg_array(k)*item;  % prod
-                            
-                        end
-               
-                        J_i   = [J_i,item];                % From left to right
-                    end
-                    
-                    for j=1:N-i                            %  zero items in certain piece      
+                for j = i:-1:1                         %  non-zero items in certain piece           
 
-                        item = zeros(6);                   
-                        
-                        J_i = [J_i,item];                                  
-                        
-                    end 
+                    item = intdAdjg_array(:,i-j+1);   
+
+                    for k = 1:j                       
+
+                        item = invAdjg_array(k)*item;  % prod
+
+                    end
+
+                    J_i   = [J_i,item];                % From left to right
+                end
+
+                for j=1:N-i                            %  zero items in certain piece      
+
+                    item = zeros(6);                   
+
+                    J_i = [J_i,item];                                  
+
+                end
                     
-               % generate the derivative of Jacobian matrix
+                % generate the derivative of Jacobian matrix
           
                 dotJ_i = [];                                       %  Equation 20
                 
-                    for j = i:-1:1                                 %  non-zero items in certain piece
-                        
-                        item = Adjg_array(:,i-j+1).* matrix_adj(eta_prec);  %  integral item
-                                    
-                        for k = 1:j                      
-                            
-                            item = invAdjg_array(k)*item;          % prod
-                            
-                        end 
-                        
-                        dotJ_i   = [dotJ_i,item]; 
-                        
-                    end 
-                        
-                    for j=1:N-i                                    %  zero items in certain piece      
-                         
-                        item     = zeros(6);
-                        
-                        dotJ_i   = [dotJ_i,item];
-                        
-                    end 
+                for j = i:-1:1                                 %  non-zero items in certain piece
+
+                    item = Adjg_array(:,i-j+1).* matrix_adj(eta_prec);  %  integral item
+
+                    for k = 1:j                      
+
+                        item = invAdjg_array(k)*item;          % prod
+
+                    end
+
+                    dotJ_i   = [dotJ_i,item]; 
+
+                end 
+
+                for j=1:N-i                                    %  zero items in certain piece      
+
+                    item     = zeros(6);
+
+                    dotJ_i   = [dotJ_i,item];
+
+                end 
                     
                  Sm = J_i(m);                                      %  Compute S. where S is the function of the abscissi X
                  
@@ -177,25 +206,30 @@ for m = 1:num_piece
                  
                  dotSn = dotJ_i(n);
                          
-                 M_mn  = M_mn + Sm' * M* Sn * dX;                                                                 % equation 31 
+                 M_mn_i  = M_mn_i + Sm' * M * Sn * dX;                                                              % equation 31 
                  
-                 C1_mn = C1_mn + Sm'* matrix_coadj(eta_prec+intdAdjg_array(:,jj)).*xidot_array(:,jj)*M* Sn * dX;  % equation 32
+                 C1_mn_i = C1_mn_i + Sm'* matrix_coadj(eta_prec_list(:,i)+intdAdjg_array(:,jj)).*xidot_array(:,jj)*M* Sn * dX;  % equation 32
                                          
 %                C2_mn = C2_mn + Sm'* M * matrix_adj(intdAdjg*xidot_array(:,jj))*Sn * dX;        
 
-                 C2_mn = C2_mn + Sm'* M *dotSn * dX;                                                              % equation 33
+                 C2_mn_i = C2_mn_i + Sm'* M * dotSn * dX;                                                              % equation 33
                  
-                 G_m   = G_m + Sm' * M * matrix_Adjoint(g_prec^-1)*dX;                                            % equation 35
+                 G_m_i   = G_m_i + Sm' * M * matrix_Adjoint(g_prec_list(:, 4*i-3:4*i)^-1)*dX;                                            % equation 35
                  
             end
-                 
+            
+            M_mn = M_mn + M_mn_i;
+            C1_mn = C1_mn + C1_mn_i;
+            C2_mn = C2_mn + C2_mn_i;
+            G_m = G_m + G_m_i;
+            
             %    recursive factors
                  
-                 invAdjg_last   =piecewise_invAdjoint(X1,theta_array(:,jj),xi_array(:,jj));
-                 intdAdjg_last  =piecewise_ADJ(X1,theta_array(:,jj),xi_array(:,jj));
-                 g_prec          =g_prec*piecewise_expmap(X1,theta_array(:,jj),xi_array(:,jj));%%---position
-                 ADxin           =intdAdjg_last*xidot_array(:,jj);
-                 eta_prec        =invAdjg_last*(eta_prec+ADxin);                               %%--speed
+%              invAdjg_last   =piecewise_invAdjoint(X1,theta_array(:,jj),xi_array(:,jj));
+%              intdAdjg_last  =piecewise_ADJ(X1,theta_array(:,jj),xi_array(:,jj));
+%              g_prec          =g_prec*piecewise_expmap(X1,theta_array(:,jj),xi_array(:,jj));%%---position
+%              ADxin           =intdAdjg_last*xidot_array(:,jj);
+%              eta_prec        =invAdjg_last*(eta_prec+ADxin);                               %%--speed
                  
             %   Actuation and internal load
                 
@@ -218,7 +252,7 @@ for m = 1:num_piece
                   ECL_m     = ECL_m + Sm' *  Fpn;                          %    Equation 30
                   
         end       
-                       
+        
         GIM(6*m-5:6*m, 6*n-5:6*n)  = M_mn;   %  Fill in the integrated block.
         GCM1(6*m-5:6*m, 6*n-5:6*n) = C1_mn;
         GCM2(6*m-5:6*m, 6*n-5:6*n) = C2_mn;
@@ -230,12 +264,11 @@ for m = 1:num_piece
 end
 
 dotz1       =Xidot;
-dotz2       =GIM^-1*(Tau+GM*matrix_Adjoint(g_r^-1)*Gra+ECL-(GCM1+GCM2)*Xidot);
+dotz2       =GIM^-1 * (Tau+GM*matrix_Adjoint(g_r^-1)*Gra+ECL-(GCM1+GCM2)*Xidot);
 dz       =[dotz1;dotz2];
 
 gv.g     = g;
 gv.eta   = eta;
-gv.nstep = nstep;
 gv.nstep = nstep+1;
 gv.GIM   = GIM;   % mass matrix
 gv.GM    = GM;    % Gravitational matrix
@@ -244,5 +277,5 @@ gv.GCM2  = GCM2;  % Coriolis matrix 2
 gv.Tau   = Tau;   % Internal force and actuation force L(F_a-F_i)
 gv.ECL   = ECL;   % External Concentrated Load
 
-end
+
 end
--- piecewise_driver.m
+++ piecewise_driver.m
@@ -15,10 +15,10 @@ mu       = 5e3;                           % [Pa*s] 粘度 5e3
 Poi      = 0.5;                           % [-] 泊松比 0.5
 G        = E/(2*(1+Poi));                 % [Pa] 剪切模量
 R        =10e-3;                         % [m] 半径 10e-3
-L        =250e-3;                        % [m] 臂长 250e-3
+L        =250e-3;                        % [m] total arm length 250e-3
 num_piece=2;                             % number of pieces 
 num_disc =10;                           
-X        =linspace(0,L,num_piece);       % 
+X        =linspace(0,L,num_piece+1);       % 
 A        =pi*R^2;                        % [m^2] 横截面积
 J        =pi*R^4/4;                      % [m^4] 横截面惯性矩（截面对y和z轴）
 I        =pi*R^4/2;                      % [m^4] 截面极惯性矩
@@ -41,7 +41,7 @@ M           =ro_arm*diag([I J J A A A]);       % screw inertia matrix
 time        =1;                          % [s]
 nsol        =time*10^2+1;                 
 tspan       =linspace(0,time,nsol);     
-dX          =L/(num_disc-1);             % delta X
+dX          =L/num_piece/(num_disc);     % TODO: check disc dx is node or length.
 
 %-------------------------------------------------------------------------
 % Actuation force (body frame)
@@ -145,60 +145,60 @@ for ii=1:num_piece
     
 end
 
-for ii=1:num_piece
-
-    figure
-    plot(t,s(:,ii))
-    grid on
-    title(strcat('torsion of piece',num2str(ii)))
-    xlabel('t [s]')
-    ylabel('s [1/m]')
-%   auxstr  =strcat('.\LAST RUN\torsione',num2str(ii),'.png');
-    print('-djpeg')
-
-    figure
-    plot(t,v(:,ii))
-    grid on
-    title(strcat('curvature on y of piece',num2str(ii)))
-    xlabel('t [s]')
-    ylabel('v [1/m]')
-%   auxstr  =strcat('.\LAST RUN\curvature',num2str(ii),'_on_y.png');
-    print('-djpeg')
-
-    figure
-    plot(t,k(:,ii))
-    grid on
-    title(strcat('curvature on z of piece',num2str(ii)))
-    xlabel('t [s]')
-    ylabel('k [1/m]')
-%   auxstr  =strcat('.\LAST RUN\curvature',num2str(ii),'_on_z.png');
-    print('-djpeg')
-
-    figure
-    plot(t,q(:,ii))
-    grid on
-    title(strcat('longitudinal strain of piece',num2str(ii)))
-    xlabel('t [s]')
-    ylabel('q [1]')
-%   auxstr  =strcat('.\LAST RUN\longitudinal_strain',num2str(ii),'.png');
-    print('-djpeg')
-
-    figure
-    plot(t,p(:,ii))
-    grid on
-    title(strcat('tras y strain of piece',num2str(ii)))
-    xlabel('t [s]')
-    ylabel('p [1]')
-%   auxstr  =strcat('.\LAST RUN\tras_y_strain',num2str(ii),'.png');
-    print('-djpeg')
-
-    figure
-    plot(t,r(:,ii))
-    grid on
-    title(strcat('tras z strain of piece',num2str(ii)))
-    xlabel('t [s]')
-    ylabel('r [1]')
-%   auxstr  =strcat('.\LAST RUN\tras_z_strain',num2str(ii),'.png');
-%   print('-dpng',auxstr)
-    print('-djpeg')
-end
+% for ii=1:num_piece
+% 
+%     figure
+%     plot(t,s(:,ii))
+%     grid on
+%     title(strcat('torsion of piece',num2str(ii)))
+%     xlabel('t [s]')
+%     ylabel('s [1/m]')
+% %   auxstr  =strcat('.\LAST RUN\torsione',num2str(ii),'.png');
+%     print('-djpeg')
+% 
+%     figure
+%     plot(t,v(:,ii))
+%     grid on
+%     title(strcat('curvature on y of piece',num2str(ii)))
+%     xlabel('t [s]')
+%     ylabel('v [1/m]')
+% %   auxstr  =strcat('.\LAST RUN\curvature',num2str(ii),'_on_y.png');
+%     print('-djpeg')
+% 
+%     figure
+%     plot(t,k(:,ii))
+%     grid on
+%     title(strcat('curvature on z of piece',num2str(ii)))
+%     xlabel('t [s]')
+%     ylabel('k [1/m]')
+% %   auxstr  =strcat('.\LAST RUN\curvature',num2str(ii),'_on_z.png');
+%     print('-djpeg')
+% 
+%     figure
+%     plot(t,q(:,ii))
+%     grid on
+%     title(strcat('longitudinal strain of piece',num2str(ii)))
+%     xlabel('t [s]')
+%     ylabel('q [1]')
+% %   auxstr  =strcat('.\LAST RUN\longitudinal_strain',num2str(ii),'.png');
+%     print('-djpeg')
+% 
+%     figure
+%     plot(t,p(:,ii))
+%     grid on
+%     title(strcat('tras y strain of piece',num2str(ii)))
+%     xlabel('t [s]')
+%     ylabel('p [1]')
+% %   auxstr  =strcat('.\LAST RUN\tras_y_strain',num2str(ii),'.png');
+%     print('-djpeg')
+% 
+%     figure
+%     plot(t,r(:,ii))
+%     grid on
+%     title(strcat('tras z strain of piece',num2str(ii)))
+%     xlabel('t [s]')
+%     ylabel('r [1]')
+% %   auxstr  =strcat('.\LAST RUN\tras_z_strain',num2str(ii),'.png');
+% %   print('-dpng',auxstr)
+%     print('-djpeg')
+% end
